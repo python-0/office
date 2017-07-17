915185d0b2860cbce13f889806ae077fbd46e95f
DinglongLi <dingl.li@hnair.com>
2017-07-17 09:25:36 +0800

    migrate sqlite3 to mysql5.6

--- office/settings.py
+++ office/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'office.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '10.8.100.111',
+        'PORT': 3306,
+        'USER': 'office',
+        'PASSWORD': 'Aa123456',
+        'NAME': 'office',
     }
 }
 
